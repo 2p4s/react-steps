be338ca5f09ba053e6c4b3e1489cc0e52766161b
Marc Ahmed <marcahmed@msn.com>
2025-06-27 23:01:38 +0100

    Event handler - Arrow method

--- src/App.js
+++ src/App.js
@@ -14,15 +14,22 @@ export default function App() {
         <div className={`${step >= 2 ? "active" : ""}`}>2</div>
         <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
+
       <p className="message">
         Step {step}: {messages[step - 1]}
       </p>
-
+      {/* use arrow method function to attach a function */}
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={() => alert("Previous")}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={() => alert("Next")}
+        >
           Next
         </button>
       </div>
