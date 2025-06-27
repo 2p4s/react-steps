500c8da3ffe544096937a90ab5ddd64fad6c2a09
Marc Ahmed <marcahmed@msn.com>
2025-06-27 23:58:12 +0100

    multiple state

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ export default function App() {
   //check import, set a default and return it
   //const arr = useState(1);
   //define the variable, its method and a default. anything with "use" is a React Hook
+  //always use the set method to update
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   //create event handlers in the component
   function handlePrevious() {
@@ -27,31 +29,40 @@ export default function App() {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    <div>
+      {/* define function here as its simple */}
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        //have to use Javascript mode as we're doing a conditional
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      {/* use arrow method function to attach a function */}
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious} //do not call it ie. handlePrevious() as it will run, needs to be just a function to it
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          {/* use arrow method function to attach a function */}
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious} //do not call it ie. handlePrevious() as it will run, needs to be just a function to it
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
